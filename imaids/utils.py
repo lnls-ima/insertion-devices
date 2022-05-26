d4f366af3ad9868252b94650a2f9edec9a089029
Lucas Henrique Francisco <61742723+lucashfrancisco@users.noreply.github.com>
2022-05-26 17:02:33 -0300

    Adding docstrings in utils module

--- imaids/utils.py
+++ imaids/utils.py
@@ -6,51 +6,72 @@ import radia as _rad
 
 
 def set_len_tol(absolute=1e-12, relative=1e-12):
-    """_summary_
+    """Set absolute and relative randomization for Radia lengths.
+
+    This is a wrapper function for the radia.FldLenTol function.
+
+    The randomizaton values are internal parameters of Radia calculations
+    for which the default values are tipically optmial. Too small values
+    may lead to errors.    
 
     Args:
-        absolute (_type_, optional): _description_. Defaults to 1e-12.
-        relative (_type_, optional): _description_. Defaults to 1e-12.
+        absolute (float, optional): Absolute randomization magnitude.
+            Defaults to 1e-12.
+        relative (float, optional): Relative randomization magnitude.
+            Defaults to 1e-12.
 
     Returns:
-        _type_: _description_
+        int: 1
     """
     return _rad.FldLenTol(absolute, relative)
 
 
 def delete_all():
-    """_summary_
+    """Deletes all Radia objects.
+
+    This is a wrapper function for the radia.UtiDelAll function.
+
+    Deletes all Radia elements, including field sources, materials,
+    transformations and interaction matrices.
 
     Returns:
-        _type_: _description_
+        int: 0
     """
     return _rad.UtiDelAll()
 
 
 def cosine_function(z, bamp, freq, phase):
-    """_summary_
+    """Cosine function with specified amplitude, frequency and phase.
 
     Args:
-        z (_type_): _description_
-        bamp (_type_): _description_
-        freq (_type_): _description_
-        phase (_type_): _description_
+        z (float or numpy.ndarray): Cosine argument parameter.
+        bamp (float): Amplitude multiplying cosine.
+        freq (float): Cosine frequency multiplying z parameter.
+        phase (float): Phase added to the cosine argument.
 
     Returns:
-        _type_: _description_
+        float or numpy.ndarray: Cosine function, bamp*cos(freq*z + phase)
     """
     return bamp*_np.cos(freq*z + phase)
 
 
 def hybrid_undulator_pole_length(gap, period_length):
-    """_summary_
+    """Hybrid undulator optmized pole thickness.
+
+    Equation and coefficients as described in:
+        J. Bahrdt, E. Gluskin, Nuclear Instruments and Methods
+        in Physics Research Section A 907, 149–168 (2018).
+        doi: 10.1016/j.nima.2018.03.069
 
     Args:
-        gap (_type_): _description_
-        period_length (_type_): _description_
+        gap (float): Magnetic gap
+            (lenght units, same as used for period_length, ex: mm).
+        period_length (float): Period length
+            (lenght units, same as used for gap, ex: mm).
 
     Returns:
-        _type_: _description_
+        float: Optmized pole thickness resulting from the applyed model
+            (lenght units, same as used for inputs, ex: mm).
     """
     a = 5.939
     b = -11.883
@@ -62,17 +83,20 @@ def hybrid_undulator_pole_length(gap, period_length):
 
 
 def fitting_matrix(tim, freqs):
-    """Create the matrix used for fitting of fourier components.
+    """Create the matrix used for fitting of Fourier components.
+
     The ordering of the matrix is the following:
-        mat[i, 2*j] = cos(2*pi*freqs[j]*tim[i])
-        mat[i, 2*j+1] = sin(2*pi*freqs[j]*tim[i])
+        mat[i, 2*j] = cos(freqs[j]*tim[i])
+        mat[i, 2*j+1] = sin(freqs[j]*tim[i])
 
     Args:
-        tim (numpy.ndarray): array with times
-        freqs (numpy.ndarray): array with frequencies to fit.
+        tim (numpy.ndarray): Array of times, or other quantity parameterizing
+            the fitted data, such as position. Defines frequency units.
+        freqs (numpy.ndarray): Array with frequencies to fit (in units of
+            the reciprocal of tim units).
 
     Returns:
-        numpy.ndarray: fitting matrix (len(tim), 2*len(freqs))
+        numpy.ndarray: Fitting matrix of shape (len(tim), 2*len(freqs))
     """
     mat = _np.zeros((tim.size, 2*freqs.size))
     arg = freqs[None, :]*tim[:, None]
@@ -87,16 +111,24 @@ def fit_fourier_components(data, freqs, tim):
     """Fit Fourier components in signal for the given frequencies.
 
     Args:
-        data (numpy.ndarray, NxM): signal to be fitted consisting of M
-            columns of data.
-        freqs (numpy.ndarray, K): K frequencies to fit Fourier components.
-        tim (numpy.ndarray, N): time vector for data columns.
+        data (numpy.ndarray, NxM): Signal to be fitted consisting of M
+            columns of data. Each element or line of data is related to
+            an element of tim.
+        freqs (numpy.ndarray, K): K frequencies to fit Fourier components
+            (in units of the reciprocal of tim units)
+        tim (numpy.ndarray, N): Time vector for data columns, or vector of
+            other quantity parameterizing the data, such as position. Defines
+            frequency units. 
 
     Returns:
-        numpy.ndarray, KxM: Fourier amplitudes.
-        numpy.ndarray, KxM: Fourier phases (phase==0 means pure sine).
+        numpy.ndarray, KxM: Fourier amplitudes, modulus of a [C_sin, C_cos]
+            vector of the cosine and sine coefficients at a given frequency.
+        numpy.ndarray, KxM: Fourier phase, argument of the same [C_sin, C_cos]
+            vector (phase==0 means pure sine).
         numpy.ndarray, KxM: Fourier cosine coefficients.
         numpy.ndarray, KxM: Fourier sine coefficients.
+        numpy.ndarray, Nx2K: Fourier matrix of sines and cosines, returned
+            by the fitting_matrix method.
     """
     mat = fitting_matrix(tim, freqs)
     coeffs, *_ = _np.linalg.lstsq(mat, data, rcond=None)
@@ -107,21 +139,32 @@ def fit_fourier_components(data, freqs, tim):
     return amps, phases, cos, sin, mat
 
 
-def calc_cosine_amplitude(
-        pos_list, values_list, freq_guess, maxfev=5000):
-    """_summary_
+def calc_cosine_amplitude(pos_list, values_list, freq_guess, maxfev=5000):
+    """Fit 3D vector components dependency on 1D positions
+         using cosine functions.
+
+    This function may be used for any 1D oscilatory dependency of 3D vector.
+    As an example (and typical use) the units described bellow assume the fit
+    of a magnetic field 1D position profile, in T and mm.
 
     Args:
-        pos_list (_type_): _description_
-        values_list (_type_): _description_
-        freq_guess (_type_): _description_
-        maxfev (int, optional): _description_. Defaults to 5000.
+        pos_list (list, N): List of positions (in mm)
+        values_list (list, Nx3): List of 3D field vectors associated with the
+            positions list. Elements are lists of the three vector components.
+            (in T)
+        freq_guess (float): Initial guess for the spacial frequencies of the
+            fitting cosines. (in 1/mm)
+        maxfev (int, optional): maximun number of calls to the least squares
+            scipy function during fitting. Default value typically enough.
+            Defaults to 5000.
 
     Raises:
-        ValueError: _description_
+        ValueError: Raised if list of values and list of positions
+            are not of same length.
 
     Returns:
-        _type_: _description_
+        list, 3: Amplitudes fitted to the components' oscilathions (in T).
+        list, 3: Phases fitted to the components' osiclations (dimensionless).
     """
     if len(pos_list) != len(values_list):
         raise ValueError(
@@ -175,34 +218,43 @@ def calc_cosine_amplitude(
 
 
 def depth(lst):
-    """_summary_
+    """Returns list depth.
 
     Args:
-        lst (_type_): _description_
+        lst (list): List for which depth is to be determined.
 
     Returns:
-        _type_: _description_
+        int: Depth of input list.
+    
+    Examples:
+        >>> depth([1,2,3])
+        1
+        >>> depth([[1,2,3]])
+        2
+        >>> depth([[[1,2],[3,4]]])
+        3
     """
     return isinstance(lst, list) and max(map(depth, lst)) + 1
 
 
 def flatten(lst):
-    """_summary_
+    """Converts 2D matrix into a 1D list.
 
     Args:
-        lst (_type_): _description_
+        lst (list): 2D matrix (list with depth=2).
 
     Returns:
-        _type_: _description_
+        list: 1D list of the input matrix elements.
     """
     return [item for sublist in lst for item in sublist]
 
 
 def get_constants():
-    """_summary_
+    """Basic physical constants for calculations on insertion devices.
 
     Returns:
-        _type_: _description_
+        float: Electron rest energy (in eV)
+        float: Speed of light (in m/s)
     """
     electron_rest_energy = 510998.92811  # [eV]
     light_speed = 299792458  # [m/s]
@@ -210,13 +262,15 @@ def get_constants():
 
 
 def calc_beam_parameters(energy):
-    """_summary_
+    """Calculates electron beam parameters from its energy.
 
     Args:
-        energy (_type_): _description_
+        energy (float): Electron energy at the beam (in KeV)
 
     Returns:
-        _type_: _description_
+        float: Beta v/c lightspeed fraction (dimensionless).
+        float: Lorentz gamma factor (dimensionless).
+        float: Electron (q=-1) magnetic rigidity (in eV.s/m).
     """
     electron_rest_energy, light_speed = get_constants()
     gamma = energy*1e9/electron_rest_energy
@@ -226,15 +280,46 @@ def calc_beam_parameters(energy):
 
 
 def newton_lorentz_equation(a, r, b):
-    """_summary_
+    """Equation of motion for a charged particle in a magnetic.
 
+    Implementarion allows for both time (t) and lenght (s) parametrization.
+    Trajectory is described by 6 components vectors, labeled "r", in which:
+        (r[0], r[1], r[2]) are the (x,y,z) cartesian coordinates
+        (r[3], r[4], r[5]) are, respectively, the derivatives of x, y and z
+            coordenates with respect to the trajectory parameter (t or s).
+    
+    The equation of motion defines the derivatative (r') of such vector:
+        r'[0], r'[1] and r'[2] are r[3], r[4], and r[5], respectively.
+        r'[3], r'[4] and r'[5] derivatives are defined by the Lorentz force:
+            (r'[3], r'[4], r'[5]) = -a (r[3], r[4], r[5]) x (b[0], b[1], b[2])            
+            where "b" is the magnetic field and "x" is the vector product
+    The negative sign at the equation makes it more convenient in calculations
+    for negatively charged electrons, avoiding negative "a" values.
+    
     Args:
-        a (_type_): _description_
-        r (_type_): _description_
-        b (_type_): _description_
-
+        a (float): Lorentz force pre-factor
+            For Lorentz magnetic force at each parametrizaton (with [units]):
+                Time parameter (t):     a = -q[C]/(gamma*m[Kg])
+                Lenght parameter (s):   a = -q[C]*c[m/s]/(beta*E[J])
+                                        a = -q[e]*c[m/s]/(beta*E[eV])
+                            for q=-1e:  a = c[m/s]/(beta*E[eV])
+                m: particle mass      
+                q: particle electric charge
+                e: elementary carge
+                c: speed of light
+                beta: lightspeed fraction, v/c
+                gamma: lorentz vactor, (1-beta^2)^(-1/2)        
+        r (list, 6): Vector containing the cartesian and their derivatives.
+            (coordinates in mm).
+            (derivatives in mm/s or dimensionless, for t or s parameters).        
+        b (list, 3): Magnetic field 3D vector (in T)
+    
     Returns:
-        _type_: _description_
+        list, 6: Derivative of the input trajectory vector, contains
+            3 cartesian first derivatives (velocities) and 3 cartesian second
+            derivatives (acceleracionts)
+                (first derivatives in mm/s or dimensionless, for t or s).
+                (second derivatives in mm/(s^2) or 1/mm, for t or s).
     """
     drds = _np.zeros(6)
     drds[0] = r[3]
@@ -247,14 +332,14 @@ def newton_lorentz_equation(a, r, b):
 
 
 def rotation_matrix(axis, theta):
-    """_summary_
+    """Returns rotation matrix for rotation by an angle around an axis
 
     Args:
-        axis (_type_): _description_
-        theta (_type_): _description_
+        axis (list, 3): Vector representing rotation axis.
+        theta (float): Rotation angle (in radians).
 
     Returns:
-        _type_: _description_
+        numpy.ndarray, 3x3: Rotation matrix.
     """
     axis = _np.asarray(axis)
     axis = axis / _np.sqrt(_np.dot(axis, axis))
@@ -271,42 +356,53 @@ def rotation_matrix(axis, theta):
 
 
 def find_peaks(data, prominence=0.05):
-    """_summary_
+    """Find the indices of peaks in data list.
+
+    Peaks are found by the function scipy.signal.find_peaks.
 
     Args:
-        data (_type_): _description_
-        prominence (float, optional): _description_. Defaults to 0.05.
+        data (list): Data list for determining peak indices.
+        prominence (float or list, 2; optional): Minimum peak prominence,
+            or, if list, minimum and maximun prominence. Defaults to 0.05.
 
     Returns:
-        _type_: _description_
+        numpy.ndarray: Indices of found data peaks. 
     """
     peaks, _ = _signal.find_peaks(data, prominence=prominence)
     return peaks
 
 
 def find_valleys(data, prominence=0.05):
-    """_summary_
+    """Find the indices of valleys in data list.
+
+    Valleys are found by the function scipy.signal.find_peaks,
+        which recieves the original data multiplied by -1.
 
     Args:
-        data (_type_): _description_
-        prominence (float, optional): _description_. Defaults to 0.05.
+        data (list): Data list for determining valley indices.
+        prominence (float or list, 2; optional): Minimum valley prominence,
+            or, if list, minimum and maximun prominence. Defaults to 0.05.
 
     Returns:
-        _type_: _description_
+        numpy.ndarray: Indices of found data valleys. 
     """
     valleys, _ = _signal.find_peaks(data*(-1), prominence=prominence)
     return valleys
 
 
 def find_peaks_and_valleys(data, prominence=0.05):
-    """_summary_
+    """Find indices of peaks and valleys in data list.
+
+    This method combines the methods find_peaks and find_valleys,
+        which are both executed with the same arguments. 
 
     Args:
-        data (_type_): _description_
-        prominence (float, optional): _description_. Defaults to 0.05.
+        data (list): Data list for determining peak and valley indices.
+        prominence (float or list, 2; optional): Minium prominence,
+            or, if list, minimum and maximun prominence. Defaults to 0.05.
 
     Returns:
-        _type_: _description_
+        numpy.ndarray: Indices of found data peaks and data valleys.
     """
     peaks = find_peaks(data, prominence=prominence)
     valleys = find_valleys(data, prominence=prominence)
@@ -314,14 +410,22 @@ def find_peaks_and_valleys(data, prominence=0.05):
 
 
 def find_zeros(pos, data):
-    """_summary_
+    """Find zero positions on data.
+
+    Consecutie data pairs with changing sign data are first determined:
+        Pair:       (pos[i], data[i]) and (pos[i+1], data[i+1])
+        In which:   data[i] x data[i+1] = -1
+    Each data pair results in a zero position between pos[i] and pos[i+1]:
+        (data[i+1]*pos[i] - data[i]*pos[i+1]) / (data[i+1] - data[i])
+    Which is the position in which the linear interpolation between the
+    points crosses the data==0 axis.
 
     Args:
-        pos (_type_): _description_
-        data (_type_): _description_
+        pos (numpy.ndarray): Positions list.
+        data (numpy.ndarray): Data list.
 
     Returns:
-        _type_: _description_
+        numpy.ndarray: List of the zeros' positions.
     """
     s = _np.sign(data)
     idxb = (s[0:-1] + s[1:] == 0).nonzero()[0]

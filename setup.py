8887aa7c35b4fbfe4fb004e83a8362ef8023751f
Fernando <fernandohds564@gmail.com>
2022-09-09 09:54:08 -0300

    Add informations to setup file and create requirements file.

--- setup.py
+++ setup.py
@@ -1,29 +1,44 @@
+"""Setup module."""
 
-import os
 from setuptools import setup
+import pkg_resources
 
 
-basedir = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(basedir, 'VERSION'), 'r') as _f:
+def get_abs_path(relative):
+    """Get absolute path of file."""
+    return pkg_resources.resource_filename(__name__, relative)
+
+
+with open(get_abs_path("README.md"), "r") as _f:
+    _long_description = _f.read().strip()
+
+with open(get_abs_path("imaids/VERSION"), "r") as _f:
     __version__ = _f.read().strip()
+
+with open(get_abs_path("requirements.txt"), "r") as _f:
+    _requirements = _f.read().strip().split("\n")
 
 
 setup(
     name='imaids',
     version=__version__,
-    description='Insertion devices package',
     author='lnls-ima',
+    description='Insertion devices package',
+    long_description=_long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lnls-ima/insertion-devices",
+    download_url="https://github.com/lnls-ima/insertion-devices",
     license='MIT License',
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+    ],
     packages=['imaids'],
     package_data={
-        'imaids':['presets/*']
+        'imaids': ['presets/*']
     },
-    install_requires=[
-        'numpy',
-        'scipy',
-        'pandas',
-        'openpyxl',
-    ],
+    install_requires=_requirements,
     test_suite='nose.collector',
     tests_require=['nose'],
     zip_safe=False)
